13408a70b0b4c31a2e5a495d25c878879472deaa
Edgar Chivichon <echivichon@visionap.com>
2014-02-12 20:39:10 -0600

    Backup complete

---
+++ BackupToCienciasComunicacion.sql
@@ -0,0 +1,5 @@
+How to do Back up
+
+expdp usuario/contraseña dumpfile=copia_oracle_export.dmp logfile=copia_oracle_export.log directory=CARPETA_EXPORT full=y
+
+expdp ADMIN/ADMINADMIN dumpfile=copia_oracle_export.dmp logfile=copia_oracle_export.log full=y
