fff05e67355df3eb05096bb6619dffd04572dbd8
Edgar Chivichon <echivichon@visionap.com>
2014-02-12 20:42:46 -0600

    url: to backup automatic

--- BackupToCienciasComunicacion.sql
+++ BackupToCienciasComunicacion.sql
@@ -3,3 +3,5 @@ How to do Back up
 expdp usuario/contraseña dumpfile=copia_oracle_export.dmp logfile=copia_oracle_export.log directory=CARPETA_EXPORT full=y
 
 expdp ADMIN/ADMINADMIN dumpfile=copia_oracle_export.dmp logfile=copia_oracle_export.log full=y
+backups
+http://www.ajpdsoft.com/modules.php?name=News&file=article&sid=560
